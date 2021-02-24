fbd3a23791e3e03d60f224537f6e4c75b4f6a48c
Aaron Jensen <aaronjensen@gmail.com>
2021-02-23 23:57:13 -0600

    Color tweaks

--- nano-theme-light.el
+++ nano-theme-light.el
@@ -19,19 +19,19 @@
 (defun nano-theme-set-light ()
   "Apply light Nano theme base."
   ;; Colors from Tailwind https://tailwindcss.com/docs/customizing-colors
-  (setq frame-background-mode    'light)
-  (setq nano-color-foreground "#1F2937") ;; Cool Gray 800
-  (setq nano-color-bold       "#374151") ;; Cool Gray 700
-  (setq nano-color-background "#FFFFFF") ;; White
-  (setq nano-color-background-alt "#F3F4F6") ;; Cool Gray 100
-  (setq nano-color-highlight  "#F9FAFB") ;; Cool Gray 50
-  (setq nano-color-critical   "#FF6F00") ;; Amber / L900
-  (setq nano-color-salient    "#673AB7") ;; Deep Purple / L500
-  (setq nano-color-strong     "#000000") ;; Black
-  (setq nano-color-popout     "#0369A1") ;; Light Blue 700
-  (setq nano-color-subtle     "#ECEFF1") ;; Blue Grey / L50
-  (setq nano-color-faded      "#6B7280") ;; Cool Gray 600
-  (setq nano-color-cursor-alt "#9CA3AF") ;; Cool Gray 400
+  (setq frame-background-mode 'light)
+  (setq nano-color-foreground          "#1F2937") ;; Cool Gray 800
+  (setq nano-color-bold                "#374151") ;; Cool Gray 700
+  (setq nano-color-background          "#FFFFFF") ;; White
+  (setq nano-color-background-alt      "#F3F4F6") ;; Cool Gray 100
+  (setq nano-color-highlight           "#F9FAFB") ;; Cool Gray 50
+  (setq nano-color-critical            "#EA580C") ;; Orange 600
+  (setq nano-color-salient             "#5B21B6") ;; Violet 800
+  (setq nano-color-strong              "#000000") ;; Black
+  (setq nano-color-popout              "#0369A1") ;; Light Blue 700
+  (setq nano-color-subtle              "#E5E7EB") ;; Cool Gray 200
+  (setq nano-color-faded               "#6B7280") ;; Cool Gray 600
+  (setq nano-color-cursor-alt          "#9CA3AF") ;; Cool Gray 400
   (setq nano-color-selected-background "#BAE6FD") ;; Light Blue 200
   )
 (nano-theme-set-light)
